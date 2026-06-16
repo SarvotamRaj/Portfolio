8d120009d1e59319fe6db2c7d2d7d165d5b9e111
Sarvotam Raj <01sarvotam@gmail.com>
2026-06-16 17:33:51 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function email(){
+window.open("mailto:01sarvotamraj@gmail.com");
+}
+
+function phone(){
+window.open("tel:+917782874023");
+}
+
+function github(){
+window.open("https://github.com/SarvotamRaj");
+}
